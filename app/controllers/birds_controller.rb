7b5f03585541a9cc6c414b3b2a437395465beaff
noah willis <n.willis.8724@gmail.com>
2023-07-23 17:31:15 -0500

    commit tree 6cf7d27c4b1fa8dbdf84d53199829d232d83927d

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -8,8 +8,14 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(params.permit(:name, :species))
     render json: bird, status: :created
+  end
+
+  private
+
+  def bird_params
+    params.permit(:name, :species)
   end
 
   # GET /birds/:id
